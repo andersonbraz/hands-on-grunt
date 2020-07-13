9e62a70d7eb5268b761d85eebb2eb101f5020bca
Anderson Braz <braz_anderson@msn.com>
2020-07-13 17:45:14 -0300

    Up File

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.expports = function(grunt){
+    /*
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        build: {
+            src: "",
+            dest: ""
+        }
+    });
+    */
+    grunt.registerTask("run", function(){
+        console.log("Running...");
+    });
+
+    grunt.registerTask("sleep", function(){
+        console.log("Sleeping...");
+    });
+
+}
