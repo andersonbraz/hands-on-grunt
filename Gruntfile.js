e739f0ead112071f7a82731ea670269c6edad5b7
Anderson Braz <braz_anderson@msn.com>
2020-07-13 20:38:42 -0300

    Up Files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,27 @@
-module.exports = function(grunt){
-    
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        connect: {
-          server: {
-            options: {
-              port: 8282,
-              hostname: "localhost",
-              base: "src",
-              keepalive: true,
-              livereload: true,
-            },
-          },
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks("grunt-contrib-connect");
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    connect: {
+      server: {
+        options: {
+          port: 8282,
+          hostname: "localhost",
+          base: "src",
+          keepalive: true,
+          livereload: true,
         },
-      });
+      },
+    },
+  });
 
-    grunt.registerTask("dev", function(){
-        console.log("Development mode ...");
-    });
+  grunt.registerTask("dev", function () {
+    console.log("Development mode ...");
+  });
 
-    grunt.registerTask("prod", function(){
-        console.log("Production mode ...");
-    });
-
+  grunt.registerTask("prod", function () {
+    console.log("Production mode ...");
+  });
 };
--- README.md
+++ README.md
@@ -24,11 +24,11 @@ Instalando as dependências de desenvolvimento.
 
 ```shell
 npm install grunt --save-dev
+npm install grunt-contrib-connect --save-dev
 npm install grunt-contrib-concat --save-dev
 npm install grunt-contrib-uglify --save-dev
 npm install grunt-saas --save-dev
 npm install grunt-war --save-dev
-npm install grunt-http-server --save-dev
 ```
 
 ## Step 04
