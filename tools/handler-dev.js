199533125a35e1d798a2634b81169e0832670304
Anderson Braz <braz_anderson@msn.com>
2020-07-14 23:07:12 -0300

    Up Files

--- tools/handler-dev.js
+++ tools/handler-dev.js
@@ -2,45 +2,42 @@ const fse = require("fs-extra");
 
 let project, version;
 
-this.cleanDist = function(){
+this.cleanDist = function () {
   fse.emptyDirSync("dist/");
 };
 
-this.createDist = function(){
-  fse.readJson('package.json', function(err, packageObj){
-
+this.createDist = function () {
+  fse.readJson("package.json", function (err, data) {
     if (err) console.error(err);
 
-    this.project = packageObj.name;
-    this.version = packageObj.version;
+    this.project = data.name;
+    this.version = data.version;
 
     // Criar Diretório de Projeto para Distribuição
-    fse.ensureDir(`dist/${this.project}`, err => {
+    fse.ensureDir(`dist/${this.project}`, function (err) {
       console.log(`Create: ${this.project}`);
     });
 
-    fse.ensureDir(`dist/${this.project}/fonts`, err => {
+    fse.ensureDir(`dist/${this.project}/fonts`, function (err) {
       console.log(`Fonts creating...`);
     });
 
-    fse.ensureDir(`dist/${this.project}/src`, err => {
+    fse.ensureDir(`dist/${this.project}/src`, function (err) {
       console.log(`Source creating...`);
     });
 
-    fse.ensureDir(`dist/${this.project}/styles`, err => {
+    fse.ensureDir(`dist/${this.project}/styles`, function (err) {
       console.log(`Source creating...`);
     });
-
   });
 };
 
-this.fillDist = function(){
-
+this.fillDist = function () {
   console.log("Start");
-  fse.readJson("config.json", (err, packageObj) => {
-    if (err) console.error(err)
-    console.log(packageObj.vendor);
+  fse.readJson("config.json", function (err, data) {
+    if (err) throw err;
+    let config = JSON.stringify(data);
+    console.log(config);
   });
   console.log("End");
-
 };
