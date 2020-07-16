6c082b4f54537e0f299fe02d93d20635ca183d68
Anderson Braz <braz_anderson@msn.com>
2020-07-15 21:06:44 -0300

    Up Files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
 
-const tools = require("./tools/handler-dev");
+const tools = require('./tools/handler-dev');
 
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks("grunt-contrib-connect");
-  grunt.loadNpmTasks("grunt-war");
-  grunt.loadNpmTasks("grunt-babel");
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-war');
+  grunt.loadNpmTasks('grunt-babel');
 
-  const package = grunt.file.readJSON("package.json");
-  const config = grunt.file.readJSON("config.json");
+  const package = grunt.file.readJSON('package.json');
+  const config = grunt.file.readJSON('config.json');
 
   grunt.initConfig({
     concat: {
@@ -20,11 +20,11 @@ module.exports = function (grunt) {
     babel: {
       options: {
         sourceMap: true,
-        presets: ["@babel/preset-env"],
+        presets: ['@babel/preset-env'],
       },
       dist: {
         files: {
-          "dist/app.js": "src/bundle-app.js",
+          'dist/app.js': 'src/bundle-app.js',
         },
       },
     },
@@ -32,8 +32,8 @@ module.exports = function (grunt) {
       server: {
         options: {
           port: 8282,
-          hostname: "localhost",
-          base: "src",
+          hostname: 'localhost',
+          base: 'src',
           keepalive: true,
           livereload: true,
         },
@@ -46,7 +46,7 @@ module.exports = function (grunt) {
           war_name: `${package.name}-${package.version}`,
           war_extras: [
             {
-              filename: "WEB-INF/weblogic.xml",
+              filename: 'WEB-INF/weblogic.xml',
               data: `<?xml version="1.0" encoding="UTF-8"?>\n<wls:weblogic-web-app xmlns:wls="http://xmlns.oracle.com/weblogic/weblogic-web-app" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd http://xmlns.oracle.com/weblogic/weblogic-web-app http://xmlns.oracle.com/weblogic/weblogic-web-app/1.9/weblogic-web-app.xsd">\n<wls:context-root>${package.name}</wls:context-root>\n</wls:weblogic-web-app>`,
             },
           ],
@@ -54,9 +54,9 @@ module.exports = function (grunt) {
         files: [
           {
             expand: true,
-            cwd: "src",
-            src: ["**"],
-            dest: "",
+            cwd: './dist/hands-on-grunt',
+            src: ['**'],
+            dest: '',
           },
         ],
       },
