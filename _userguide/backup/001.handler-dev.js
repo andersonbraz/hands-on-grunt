4f1b4bdbd2aa0b328a92640415f0a19a22671b12
Anderson Braz <braz_anderson@msn.com>
2020-07-16 09:42:18 -0300

    Up Files

---
+++ _userguide/backup/001.handler-dev.js
@@ -0,0 +1,81 @@
+const fse = require("fs-extra");
+
+this.cleanDev = function () {
+  fse.emptyDirSync("src/assets");
+};
+
+this.createDev = function () {
+  
+  // Criar Diretório CSS
+  fse.ensureDir('src/assets/css')
+  .then(function(){
+    console.log('Directory styles created...');
+  })
+  .catch(function(err){
+    console.error(err);
+  });
+
+  // Criar Diretório JS
+  fse.ensureDir('src/assets/js')
+  .then(function(){
+    console.log('Directory scripts created...');
+  })
+  .catch(function(err){
+    console.error(err);
+  });
+
+  // Criar Diretório FONTS
+  fse.ensureDir('src/assets/fonts')
+  .then(function(){
+    console.log('Directory fonts created...');
+  })
+  .catch(function(err){
+    console.error(err);
+  });
+
+  console.log("P1");
+
+};
+
+this.fillDev = function(data){
+
+  const vendorCss = data.vendor.css;
+  const vendorJs = data.vendor.js;
+
+  // Mounted Vendor
+  vendorCss.forEach(function(element, index, array){
+    fse.copy(element.pack, element.target, function(err){
+      if (err) return console.error(err);
+      console.log(`Copied: ${element.target}`);
+    }); 
+    //copies file
+    /*
+    fse.copy(`${element.pack}`, `${element.target}`)
+    .then(function(){
+      console.log(`Writing: ${element.target}`));
+    })
+    .catch(function(err){
+      console.error(err);
+    });
+    */
+  });
+
+  vendorJs.forEach(function(element, index, array){
+    fse.copy(element.pack, element.target, function(err){
+      if (err) return console.error(err);
+      console.log(`Copied: ${element.target}`);
+    }); 
+    /*
+    fse.copy(`${element.pack}`, `${element.target}`)
+    .then(function(){
+      console.log(`Writing: ${element.target}`));
+    })
+    .catch(function(err){
+      console.error(err);
+    });
+    */
+  });
+
+  console.log("P2");
+  
+};
